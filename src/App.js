69e878538382b47e64c1115cf0b4d01e9d80b383
Thong Le <lebathong188@gmail.com>
2022-03-15 22:19:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+
+import CardList from "./components/card-list/card-list.component";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Called before component is mounted
+  // Purposes:
+  // 1. Initializing local state by assigning an object to this.state.
+  // 2. Binding event handler methods to an instance.
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+    console.log('construct')
+  }
+
+  // Invoked immediately after a component is placed on the DOM
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => {
+          return {monsters: users};
+        },
+        () => {
+          console.log(this.state);
+        }
+      ))
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {searchField};
+    });
+  }
+
+  render() {
+    console.log('render')
+
+    // Destructuring
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    // Filter the original monsters array with a component-wide search string
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className='App'>
+        {/*Search Bar*/}
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monster'
+          onChange={onSearchChange}
+        />
+        {
+          filteredMonsters.map((monster) => {
+            return (
+              // unique key to differentiate components efficiently
+              // when re-rendering
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
+          })
+        }
+        <CardList/>
+      </div>
+    )
+  }
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//
+//     // initializing local state in
+//     // a json object
+//     this.state = {
+//       name: {firstName: 'Tong', lastName: 'Lay'},
+//       company: 'SDSU'
+//     };
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo"/>
+//           <p>
+//             Hello, my name is {this.state.name.firstName} {this.state.name.lastName}
+//           </p>
+//           <button
+//             onClick={() => {
+//               // Shallow merge into current state -> new state object
+//               // check through state object if there's the same key (name)
+//               // and update its value
+//               // Occurs asynchronously
+//               // this.setState({name: {firstName: 'Eug', lastName: 'Not'}})
+//               // The state is not updated here
+//               // console.log(this.state)
+//
+//               // Pass a function instead of directly setState
+//               this.setState(
+//                 () => {
+//                   return {
+//                     name: {firstName: 'Eug', lastName: 'Not'}
+//                   }
+//                 },
+//                 // callback function (optional)
+//                 // once all state changes have been applied, run it
+//                 () => {
+//                   console.log(this.state)
+//                 })
+//             }}
+//           >
+//             Change Name
+//           </button>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import {Component} from "react";
+
+class CardList extends Component {
+
+  render() {
+    return (
+      <div>
+        Test CardList Component
+      </div>
+    )
+  }
+}
+
+export default CardList;
