f09b3c2b2cdcb1cedb7a3886c35d7b1a1c95d2ec
Thong Le <lebathong188@gmail.com>
2022-03-26 19:11:16 -0700

    converted App from class to functional component
    wrap fetch and filtered list of monsters in useEffect() method to control execution every time App re-renders

--- src/App.js
+++ src/App.js
@@ -1,71 +1,114 @@
-import {Component} from 'react';
+// import {Component} from 'react';
+
+import {useState, useEffect} from "react";
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBar from "./components/search-bar/search-bar.component";
 import './App.css';
 
-class App extends Component {
-  // Called before component is mounted
-  // Purposes:
-  // 1. Initializing local state by assigning an object to this.state.
-  // 2. Binding event handler methods to an instance.
-  constructor(props) {
-    super(props);
+const App = () => {
+  // useState() returns an array of 2 values [value, setValue]
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-    console.log('construct');
-  }
-
-  // Invoked immediately after a component is placed on the DOM
-  componentDidMount() {
-    console.log('componentDidMount')
+  // effect has empty deps, because we only want to run it once
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(
-        () => {
-          return {monsters: users};
-        },
-        () => {
-          console.log(this.state);
-        }
-      ))
-  }
+      .then((users) => setMonsters(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return {searchField};
-    });
-  }
-
-  render() {
-    console.log('render');
-
-    // Destructuring
-    const {monsters, searchField} = this.state;
-    const {onSearchChange} = this;
-
-    // Filter the original monsters array with a component-wide search string
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
-    return (
-      <div className='App'>
-        <h1 className='app-title'>Robodex</h1>
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
-        <SearchBar
-          className='monsters-search-bar'
-          placeholder='search monster'
-          onChangeHandler={onSearchChange}/>
-
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    );
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   }
+
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Robodex</h1>
+
+      <SearchBar
+        className='monsters-search-bar'
+        placeholder='search monster'
+        onChangeHandler={onSearchChange}/>
+
+      <CardList monsters={filteredMonsters}/>
+    </div>
+  )
 }
+
+// class App extends Component {
+//   // Called before component is mounted
+//   // Purposes:
+//   // 1. Initializing local state by assigning an object to this.state.
+//   // 2. Binding event handler methods to an instance.
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       monsters: [],
+//       searchField: ''
+//     };
+//     console.log('construct');
+//   }
+//
+//   // Invoked immediately after a component is placed on the DOM
+//   componentDidMount() {
+//     console.log('componentDidMount')
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) => this.setState(
+//         () => {
+//           return {monsters: users};
+//         },
+//         () => {
+//           console.log(this.state);
+//         }
+//       ))
+//   }
+//
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return {searchField};
+//     });
+//   }
+//
+//   render() {
+//     console.log('render');
+//
+//     // Destructuring
+//     const {monsters, searchField} = this.state;
+//     const {onSearchChange} = this;
+//
+//     // Filter the original monsters array with a component-wide search string
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+//
+//     return (
+//       <div className='App'>
+//         <h1 className='app-title'>Robodex</h1>
+//
+//         <SearchBar
+//           className='monsters-search-bar'
+//           placeholder='search monster'
+//           onChangeHandler={onSearchChange}/>
+//
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
